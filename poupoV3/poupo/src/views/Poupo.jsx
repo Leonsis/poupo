698f0efce40420d3d831276e694b1a8130b25e2a
Leonsis <caiolenni@gmail.com>
2025-04-18 14:10:14 -0300

    Enviando atualizações do projeto

--- poupoV3/poupo/src/views/Poupo.jsx
+++ poupoV3/poupo/src/views/Poupo.jsx
@@ -29,7 +29,29 @@ function Poupo() {
                     <div className="row">
                         <div className="col-sm-12">
                             <div className="testo">
-                                
+                                <form id="formOne" className="row justify-content-center">
+                                    <div className="col-md-4 ">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Quantas metas você possui?</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                                        </div>
+                                        <div class="form-group bloco">
+                                            <label for="exampleInputEmail1">Qual valor você vai investir por mês para essas metas?</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                                        </div>
+                                    </div>
+                                    
+                                    <div className="col-md-4 ">
+                                        <div class="form-group">
+                                            <label for="exampleInputEmail1">Quantas metas você possui?</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                                        </div>
+                                        <div class="form-group bloco">
+                                            <label for="exampleInputEmail1">Qual valor você vai investir por mês para essas metas?</label>
+                                            <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+                                        </div>
+                                    </div>
+                                </form>                                
                             </div>
                         </div>
                     </div>
