885ab17ec320b454852e85595751a009ab3ca9fe
Leonsis <caiolenni@gmail.com>
2025-06-19 14:14:16 -0300

    Enviando atualizações

--- poupoV3/poupo/src/views/Footer.jsx
+++ poupoV3/poupo/src/views/Footer.jsx
@@ -9,10 +9,29 @@ function Footer() {
     <>
       <footer>
         <div className="container">
-          <div className='d-flex align-items-baseline'>
-            <img src={poupoLogo}/>
-            <h1>Contas</h1>
+          
+          <div className='row d-flex align-items-baseline'>
+            <div className='col-md-3'>
+              <div className='d-flex align-items-baseline'>
+                <img src={poupoLogo}/>
+                <h2 style={{ fontWeight: "bold", fontSize: "2.5rem"}}>Contas</h2>
+              </div>
+            </div>
+
+            <div className='col-md-4'>
+              <div>
+                <h2>Site map</h2>
+                <ul>                  
+                  <li>
+                    Home
+                  </li>
+                  <li>Login</li>
+                  <li>Planejamento</li>
+                </ul>
+              </div>
+            </div>
           </div>
+
           <div>
             <p>© 2024 - 2025 Feito por Caio Leonni os direitos reservados.</p>
           </div>          
